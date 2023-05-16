feb473e1303c0a0bea2f929d80726ed8e46f03af
JustiTheGreat <justinian.zaharia@gmail.com>
2023-05-16 18:18:44 +0300

    now server controller returns error codes

--- GeoChat.Geolocation/Controllers/ServerController.cs
+++ GeoChat.Geolocation/Controllers/ServerController.cs
@@ -1,6 +1,7 @@
 ﻿using GeoChat.Geolocation.Api.Entities;
 using GeoChat.Geolocation.Api.Repo;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeoChat.Geolocation.Api.Controllers;
@@ -22,36 +23,36 @@ public class ServerController : ControllerBase
 
 	[HttpGet("{latitude}/{longitude}")]
 	[Authorize]
-	public async Task<Server> GetServerUrl(double latitude, double longitude)
+	public async Task<IActionResult> GetServerUrl(double latitude, double longitude)
 	{
-		if (latitude < -90 || latitude > 90) throw new ArgumentException("Wrong latitude value!");
-		if (longitude < -180 || latitude > 180) throw new ArgumentException("Wrong longitude value!");
-		String geoHashCode = geoHasher.getGeoHashCode(latitude, longitude);
+		if (latitude < -90 || latitude > 90) return BadRequest();
+		if (longitude < -180 || latitude > 180) return BadRequest();
+        String geoHashCode = geoHasher.getGeoHashCode(latitude, longitude);
 
 		IEnumerable<Location> locations = await locationsRepository.GetAllAsync();
 		Location location = locations.First(l => geoHashCode.StartsWith(l.GeoHashCode));
-		if (location == null) return null;
+		if (location == null) return NotFound();
 
 		Server? server = await serversRepository.GetAsync(location.ServerId);
-		if (server == null) return null;
-		return server;
+		if (server == null) return NotFound();
+		return Ok(server);
 	}
 
 	[HttpGet("{id}")]
 	[Authorize]
-	public async Task<Server> GetServerUrl(long id)
+	public async Task<IActionResult> GetServerUrl(long id)
 	{
 		Server? server = await serversRepository.GetAsync(id);
-		if (server == null) return null;
-		return server;
-	}
+		if (server == null) return NotFound();
+		return Ok(server);
+    }
 
 	[HttpGet("servers")]
 	[Authorize]
-	public async Task<IEnumerable<Server>> GetServers()
+	public async Task<IActionResult> GetServers()
 	{
 		IEnumerable<Server> servers = await serversRepository.GetAllAsync();
-		if (servers == null) return null;
-		return servers;
-	}
+		if (servers == null) return NotFound();
+		return Ok(servers);
+    }
 }
